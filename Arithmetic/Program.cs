729c664f742785db562166d5f659908899b56a0e
lokeshbhusal <bhusallokesh19@gmail.com>
2018-09-27 10:11:26 +0545

    initial commit

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 6;
+            double b = 8;
+            double sum = Add(a, b);
+            Console.WriteLine("sum={0}",sum);
+      
+        }
+        static double Add(double a,double b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
